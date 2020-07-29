56a4c1ff652dd9d18aed4c8da963e5be1f3e2f5b
Igor Steinmacher <igorsteinmacher@gmail.com>
2020-07-28 21:06:42 -0700

    Moving back to a bad version to ensure tests are working

--- v0/prime.py
+++ v0/prime.py
@@ -1,13 +1,5 @@
 def is_prime(number): 
-   for element in range(2,number): 
+   for element in range(number): 
       if (number % element == 0): 
          return False
    return True 
-
-
-def print_next_prime(number): 
-   index = number 
-   while True: 
-      index += 1 
-      if is_prime(index):  
-         print(index)
