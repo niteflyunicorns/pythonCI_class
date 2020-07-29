9a2cfe78963b18f2bc3d533d34a2ec50b78d9ffe
Igor Steinmacher <igorsteinmacher@gmail.com>
2020-07-28 20:52:38 -0700

    Add files via upload

---
+++ v0/prime.py
@@ -0,0 +1,14 @@
+def is_prime(number): 
+	for element in range(number): 
+		if (number % element == 0): 
+			return False
+	return True 
+
+
+def print_next_prime(number): 
+	index = number 
+	while True: 
+		index += 1 
+		if is_prime(index): 
+			print(index)
+
