2606f1bf16f280fd58e612c4a8fcf16ff464783d
Igor Steinmacher <igorsteinmacher@gmail.com>
2020-10-13 15:45:17 -0700

    Update prime.py

--- v0/prime.py
+++ v0/prime.py
@@ -1,10 +1,8 @@
 def is_prime(number): 	
 	if (not type(number) is int):
 		return False
-
 	if (number <= 1):
 		return False
-	
 	for element in range(2,number): 
 		if (number % element == 0):
 			return False
