a6872745008cf2b396d5c2f7356f3557c228cd0b
Italo Santos <italo_07_@hotmail.com>
2024-09-17 17:57:25 +0200

    Update PrimesTestCase.py

--- v0/PrimesTestCase.py
+++ v0/PrimesTestCase.py
@@ -4,20 +4,7 @@ from prime import is_prime
 class PrimesTestCase(unittest.TestCase):
    def test_is_five_prime(self):    
       self.assertTrue(is_prime(5)) 
-   def test_is_nine_prime(self):
-      self.assertFalse(is_prime(9))
-   def test_is_negative_prime(self):
-      self.assertFalse(is_prime(-1))
-   def test_is_zero_prime(self):
-      self.assertFalse(is_prime(0))
-   def test_is_one_prime(self):
-      self.assertFalse(is_prime(1))
-   def test_is_two_prime(self):
-      self.assertTrue(is_prime(2))
-   def test_is_decimal_treated(self):
-      self.assertFalse(is_prime(5.2))
-   def test_is_decimal_treated(self):
-     self.assertFalse(is_prime("5"))
+   
 
 if __name__ == '__main__':
 	unittest.main()
