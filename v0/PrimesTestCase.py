24447bbc90ac76ea2fe636c5bc14cb374c634b8f
Italo Santos <italo_07_@hotmail.com>
2024-09-17 18:10:11 +0200

    Update PrimesTestCase.py

--- v0/PrimesTestCase.py
+++ v0/PrimesTestCase.py
@@ -8,6 +8,9 @@ class PrimesTestCase(unittest.TestCase):
    def test_is_nine_prime(self):
       self.assertFalse(is_prime(9))
 
+   def test_non_integer_input(self):
+        self.assertFalse(is_prime("10"))
+
    
 
 if __name__ == '__main__':
